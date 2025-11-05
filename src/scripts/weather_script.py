e03f10330bcdb5229c0a35520873d51c11fe98aa
PapsBurr <npons3932@gmail.com>
2025-11-05 15:52:34 -0700

    Add the weather icon to the get_weather method

--- src/scripts/weather_script.py
+++ src/scripts/weather_script.py
@@ -1,5 +1,6 @@
 import requests
 import logging
+import os
 
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
@@ -21,12 +22,14 @@ class WeatherScript:
         if response.status_code == 200:
             data = response.json()
             logging.debug(f"Weather Data Received: {data}")
+            data_icon_code = data['weather'][0]['icon']
+            icon_path = self.get_icon_path(data_icon_code)
             return {
+                "icon_path": icon_path,
                 "weather": data['weather'][0]['main'],
                 "temperature": data['main']['temp'],
                 "humidity": data['main']['humidity'],
                 "description": data['weather'][0]['description'],
-
             }
         elif response.status_code == 404:
             logging.error(f"City '{city}' not found.")
@@ -34,3 +37,19 @@ class WeatherScript:
         else:
             logging.error(f"Error fetching weather data: {response.status_code}")
             return {"error": response.status_code}
+    
+    def get_icon_path(self, icon_code):
+        icon_path = f"src/assets/weather/weather_icon_{icon_code}.png"
+        if os.path.exists(icon_path):
+            logging.debug(f"Icon already exists at path: {icon_path}")
+            return icon_path
+        else:
+            response = requests.get(f"http://openweathermap.org/img/wn/{icon_code}@2x.png")
+            if response.status_code == 200:
+                with open(icon_path, "wb") as icon_file:
+                    icon_file.write(response.content)
+                logging.debug(f"Icon downloaded and saved at path: {icon_path}")
+                return icon_path
+            else:
+                logging.error(f"Error fetching icon: {response.status_code}")
+                return None
