0a111e8fcb03b57febc067ecbee59d9588572c0e
PapsBurr <npons3932@gmail.com>
2025-11-13 20:02:58 -0700

    Rewrite to use Lambda backend

--- src/scripts/recipe_script.py
+++ src/scripts/recipe_script.py
@@ -1,3 +1,5 @@
+import json
+import os
 import requests
 import logging
 from PIL import Image
@@ -9,41 +11,62 @@ logging.basicConfig(
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 logging.getLogger("requests").setLevel(logging.WARNING)
 
+
 class RecipeScript:
-    BASE_URL = "https://api.spoonacular.com/recipes"
     MAXIMIZE_USED_INGREDIENTS = 1
     MINIMIZE_MISSING_INGREDIENTS = 2
 
-    def __init__(self, api_key):
-        self.api_key = api_key
+    def __init__(self):
         self.ignore_pantry = True  # Whether to ignore typical pantry items, such as water, salt, flour, etc.
+
+        config_path = os.path.join(
+            os.path.dirname(__file__), "..", "config", "api_config.json"
+        )
+        logging.debug(f"Loading configuration from: {config_path}")
+
+        try:
+            with open(config_path, "r") as f:
+                config = json.load(f)
+                self.base_url = f"{config['api_gateway_url']}"
+                logging.debug(f"API Gateway URL set to: {self.base_url}")
+
+        except FileNotFoundError:
+            raise FileNotFoundError(
+                f"Configuration file not found at {config_path}. Please ensure the file exists and try again."
+            )
+        except KeyError:
+            raise KeyError(
+                "API_GATEWAY_URL not found in configuration. Please check the config file."
+            )
+        except Exception as e:
+            raise Exception(f"An error occurred while loading configuration: {e}")
 
     def get_recipes(self, ingredients, number=1):
         params = {
-            "apiKey": self.api_key,
             "ingredients": ingredients,
             "number": number,
             "ignorePantry": str(self.ignore_pantry).lower(),
             "ranking": self.MAXIMIZE_USED_INGREDIENTS,
         }
         try:
-            response = requests.get(f"{self.BASE_URL}/findByIngredients", params=params)
+            response = requests.get(f"{self.base_url}/recipe", params=params)
             if response.status_code == 200:
                 data = response.json()
                 logging.debug(f"Recipes found: {data}")
                 if data:
                     return data
                 else:
-                    return None
+                    return json.dumps({"error": "No recipes found."})
             else:
                 response.raise_for_status()
                 logging.error(f"Error finding recipes: {response.status_code}")
         except Exception as e:
             logging.error(f"Exception occurred while finding recipes: {e}")
-            return None
+            return json.dumps({"error": "Error finding recipes."})
 
     def get_recipe_image(self, image_url):
         if image_url:
+
             try:
                 response = requests.get(image_url)
                 response.raise_for_status()
@@ -52,7 +75,7 @@ class RecipeScript:
                 return pil_image
             except Exception as e:
                 logging.error(f"Error fetching recipe image: {e}")
-                return None
+                return json.dumps({"error": "Error fetching recipe image."})
         else:
             logging.warning("No image URL provided for recipe.")
-            return None
+            return json.dumps({"error": "No image URL provided for recipe."})
--- src/scripts/weather_script.py
+++ src/scripts/weather_script.py
@@ -1,3 +1,4 @@
+import json
 import requests
 import logging
 import os
@@ -8,45 +9,71 @@ logging.basicConfig(
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 logging.getLogger("requests").setLevel(logging.WARNING)
 
+
 class WeatherScript:
-    def __init__(self, api_key):
-        self.api_key = api_key
-        self.base_url = "https://api.openweathermap.org/data/2.5/weather"
+    def __init__(self):
+        config_path = os.path.join(
+            os.path.dirname(__file__), "..", "config", "api_config.json"
+        )
+        logging.debug(f"Loading configuration from: {config_path}")
+
+        try:
+            with open(config_path, "r") as f:
+                config = json.load(f)
+                self.base_url = f"{config['api_gateway_url']}/weather"
+                logging.debug(f"API Gateway URL set to: {self.base_url}")
+
+        except FileNotFoundError:
+            raise FileNotFoundError(
+                f"Configuration file not found at {config_path}. Please ensure the file exists and try again."
+            )
+        except KeyError:
+            raise KeyError(
+                "API_GATEWAY_URL not found in configuration. Please check the config file."
+            )
+        except Exception as e:
+            raise Exception(f"An error occurred while loading configuration: {e}")
 
     def get_weather(self, location):
-        params = {
-            'q': location,
-            'appid': self.api_key,
-            'units': 'metric'
-        }
+        params = {"location": location}
         logging.debug(f"Requesting weather data for location: {location}")
-        response = requests.get(self.base_url, params=params)
-        if response.status_code == 200:
-            data = response.json()
-            logging.debug(f"Weather Data Received: {data}")
-            data_icon_code = data['weather'][0]['icon']
-            icon_path = self.get_icon_path(data_icon_code)
-            return {
-                "icon_path": icon_path,
-                "weather": data['weather'][0]['main'],
-                "temperature": data['main']['temp'],
-                "humidity": data['main']['humidity'],
-                "description": data['weather'][0]['description'],
-            }
-        elif response.status_code == 404:
-            logging.error(f"Location '{location}' not found.")
-            return {"error": "Location not found"}
-        else:
-            logging.error(f"Error fetching weather data: {response.status_code}")
-            return {"error": response.status_code}
-    
+
+        try:
+            response = requests.get(self.base_url, params=params)
+            if response.status_code == 200:
+                data = response.json()
+                logging.debug(f"Weather Data Received: {data}")
+                data_icon_code = data["weather"][0]["icon"]
+                icon_path = self.get_icon_path(data_icon_code)
+                return {
+                    "icon_path": icon_path,
+                    "weather": data["weather"][0]["main"],
+                    "temperature": data["main"]["temp"],
+                    "humidity": data["main"]["humidity"],
+                    "description": data["weather"][0]["description"],
+                }
+            elif response.status_code == 404:
+                logging.error(f"Location '{location}' not found.")
+                return {"error": "Location not found"}
+            else:
+                logging.error(f"Error fetching weather data: {response.status_code}")
+                return {"error": response.status_code}
+        except requests.exceptions.RequestException as e:
+            logging.error(f"RequestException during API request: {e}")
+            return {"error": "Request exception occurred while fetching data"}
+        except Exception as e:
+            logging.error(f"Exception during API request: {e}")
+            return {"error": "Exception occurred while fetching data"}
+
     def get_icon_path(self, icon_code):
         icon_path = f"src/assets/weather/weather_icon_{icon_code}.png"
         if os.path.exists(icon_path):
             logging.debug(f"Icon already exists at path: {icon_path}")
             return icon_path
         else:
-            response = requests.get(f"http://openweathermap.org/img/wn/{icon_code}@2x.png")
+            response = requests.get(
+                f"http://openweathermap.org/img/wn/{icon_code}@2x.png"
+            )
             if response.status_code == 200:
                 with open(icon_path, "wb") as icon_file:
                     icon_file.write(response.content)
