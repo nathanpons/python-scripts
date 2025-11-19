e8bfc33abdf7220d8fb47ed52b3d1e0ff980baa5
PapsBurr <npons3932@gmail.com>
2025-11-19 11:14:59 -0700

    Add auto use fixture to block network get requests in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,8 @@ Pytest configuration file.
 """
 
 import sys
+import pytest
+import requests
 from pathlib import Path
 from unittest.mock import MagicMock
 
@@ -10,3 +12,9 @@ project_root = Path(__file__).parent.parent
 sys.path.insert(0, str(project_root))
 
 sys.modules['customtkinter'] = MagicMock()
+
+@pytest.fixture(autouse=True)
+def disable_network_calls(monkeypatch):
+    def stunted_get():
+        raise RuntimeError("Network calls are disabled during tests.")
+    monkeypatch.setattr(requests, "get", lambda *args, **kwargs: stunted_get())
