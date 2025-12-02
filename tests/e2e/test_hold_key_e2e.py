76c9093ba4b2a8e449b01e0caf4c47562cd1e558
PapsBurr <npons3932@gmail.com>
2025-12-02 09:55:07 -0700

    test: add beginning of e2e tests for hold_key workflow

---
+++ tests/e2e/test_hold_key_e2e.py
@@ -0,0 +1,17 @@
+"""
+Test cases for the Hold Key end-to-end functionality.
+"""
+
+import pytest
+import time
+import pyautogui
+from gui.hold_key_ui import HoldKeyUI
+
+class TestHoldKeyE2E:
+    """End-to-end tests for the Hold Key functionality."""
+    pass
+
+
+
+
+
